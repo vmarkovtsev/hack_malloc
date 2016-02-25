91b7a907edb7002163471b10db5b7569268e6fc7
Vadim Markovtsev <v.markovtsev@samsung.com>
2016-02-25 18:04:20 +0300

    Change hack_malloc to be a preamble-based allocator

--- hack_malloc.c
+++ hack_malloc.c
@@ -5,40 +5,25 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
-typedef struct {
-  void* addr;
-  size_t size;
-} MemoryInfo;
-
-static MemoryInfo infos[100];
-
-static int info_pos = 0;
-
 void* malloc(size_t size) {
   printf("malloc\n");
-  if (info_pos >= sizeof(infos) / sizeof(MemoryInfo)) {
-    return NULL;
-  }
+  size += sizeof(size_t);
   int page_size = getpagesize();
   int rem = size % page_size;
   if (rem > 0) {
     size += page_size - rem;
   }
   void* addr = mmap(0, size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
-  infos[info_pos].addr = addr;
-  infos[info_pos].size = size;
-  info_pos++;
-  return addr;
+  if (addr == MAP_FAILED) {
+    return NULL;
+  }
+  *(size_t*)addr = size;
+  return (size_t*)addr + 1;
 }
 
 void free (void *ptr) {
   printf("free... ");
-  for (int i = info_pos - 1; i >= 0; i--) {
-    if (infos[i].addr == ptr) {
-      printf("ok");
-      munmap(ptr, infos[i].size);
-      break;
-    }
-  }
+  size_t* real_ptr = (size_t*)ptr - 1;
+  munmap(real_ptr, *real_ptr);
   printf("\n");
 }
