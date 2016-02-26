29088dba322436f058a11f701eaad86a1f168b87
Vadim Markovtsev <v.markovtsev@samsung.com>
2016-02-27 00:23:29 +0300

    Check and fix MacOSX El Capitan malloc override

--- README.md
+++ README.md
@@ -23,5 +23,6 @@ LD_PRELOAD=/path/to/liboverridemalloc.so ./program
 ```
 Mac OS X:
 ```
-DYLD_INSERT_LIBRARIES=/path/to/liboverridemalloc.so ./program
+DYLD_INSERT_LIBRARIES=/path/to/liboverridemalloc.dylib DYLD_FORCE_FLAT_NAMESPACE=1 ./program
 ```
+Note for Mac OS X: due to [SIP](https://en.wikipedia.org/wiki/System_Integrity_Protection), programs at /usr/bin, e.g., ls, are not going to be affected.
--- override_malloc.c
+++ override_malloc.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <dlfcn.h>
-
 #include <unistd.h>
-#include <sys/mman.h>
 
 void* malloc(size_t size) {
-  printf("malloc\n");
+  write(STDOUT_FILENO, "malloc\n", 7);
+  // printf("malloc\n"); leads to Segmentation Fault on MacOSX
   return ((void*(*)(size_t))dlsym (RTLD_NEXT, "malloc"))(size);
 }
 
 void free (void *ptr) {
-  printf("free\n");
+  write(STDOUT_FILENO, "free\n", 5);
   ((void(*)(void*))dlsym(RTLD_NEXT, "free"))(ptr);
 }
